9cf56ec0c66bb6aa6a70c58d5ed5c76c478acc50
sohui.dev <113837605+qwerty00ui88@users.noreply.github.com>
2025-01-04 00:34:12 +0900

    feat: 사용자 회원가입 기능 추가
    - /signup GET 요청으로 회원가입 화면 이동 기능 추가
    - 회원가입 데이터를 처리하는 POST 요청 핸들러 추가
      - AccountDto를 Account 엔티티로 변환 후 저장
      - 비밀번호를 PasswordEncoder로 암호화 처리
    - ModelMapper를 활용해 DTO와 엔티티 간 매핑 구현
    - UserService와 UserRepository를 통한 데이터베이스 연동
    - PostgreSQL 설정 추가 (datasource 및 JPA 설정)

--- src/main/java/io/security/springsecuritymaster/config/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,8 +19,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon--*").permitAll().requestMatchers("/").permitAll().anyRequest().authenticated()).formLogin(form -> form.loginPage("/login").permitAll());
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon--*").permitAll().requestMatchers("/", "/signup").permitAll().anyRequest().authenticated()).formLogin(form -> form.loginPage("/login").permitAll());
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Bean
