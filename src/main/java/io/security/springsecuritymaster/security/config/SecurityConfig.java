d68f02cacd4cf5fef608a1da4f62b73961fee416
sohui.dev <113837605+qwerty00ui88@users.noreply.github.com>
2025-01-04 16:39:46 +0900

    feat: 추가 인증 요소(secret_key) 검증 로직 구현
    - FormAuthenticationDetails 및 AuthenticationDetailsSource 구현
    - SecurityConfig에 AuthenticationDetailsSource 등록
    - 로그인 폼에 `secret_key` 숨겨진 필드 추가
    - 잘못된 `secret_key` 입력 시 SecretException 처리

--- src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package io.security.springsecuritymaster.security.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +16,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
 @EnableWebSecurity
 @Configuration
@@ -21,10 +24,11 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final AuthenticationProvider authenticationProvider;
+    private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon--*").permitAll().requestMatchers("/", "/signup").permitAll().anyRequest().authenticated()).formLogin(form -> form.loginPage("/login").permitAll()).authenticationProvider(authenticationProvider);
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon--*").permitAll().requestMatchers("/", "/signup").permitAll().anyRequest().authenticated()).formLogin(form -> form.loginPage("/login").permitAll().authenticationDetailsSource(authenticationDetailsSource)).authenticationProvider(authenticationProvider);
         return http.build();
     }
 
