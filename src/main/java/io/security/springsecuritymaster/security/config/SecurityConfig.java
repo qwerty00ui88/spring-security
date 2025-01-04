4819ef43a44e2b73953e65e6c41dd4236fe0996e
sohui.dev <113837605+qwerty00ui88@users.noreply.github.com>
2025-01-04 18:03:43 +0900

    feat: 커스텀 AuthenticationSuccessHandler 구현 및 설정 추가
    - FormAuthenticationSuccessHandler 클래스 추가
      - 인증 성공 시 SavedRequest 기반으로 리다이렉트 처리
      - 저장된 요청이 없을 경우 기본 URL("/")로 리다이렉트
    - SecurityConfig 수정
      - formLogin 설정에 successHandler 추가
      - 인증 성공 후 커스텀 로직 적용

--- src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
 @EnableWebSecurity
@@ -24,11 +25,12 @@ import org.springframework.security.web.authentication.WebAuthenticationDetails;
 public class SecurityConfig {
 
     private final AuthenticationProvider authenticationProvider;
+    private final AuthenticationSuccessHandler successHandler;
     private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon--*").permitAll().requestMatchers("/", "/signup").permitAll().anyRequest().authenticated()).formLogin(form -> form.loginPage("/login").permitAll().authenticationDetailsSource(authenticationDetailsSource)).authenticationProvider(authenticationProvider);
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon--*").permitAll().requestMatchers("/", "/signup").permitAll().anyRequest().authenticated()).formLogin(form -> form.loginPage("/login").permitAll().authenticationDetailsSource(authenticationDetailsSource).successHandler(successHandler)).authenticationProvider(authenticationProvider);
         return http.build();
     }
 
