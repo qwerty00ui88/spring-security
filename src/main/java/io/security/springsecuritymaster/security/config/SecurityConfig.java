e1291a89c4769742b79922a263b6fdb348cb610c
sohui.dev <113837605+qwerty00ui88@users.noreply.github.com>
2025-01-04 11:35:29 +0900

    feat: UserDetailsService 커스터마이징 및 In-Memory 방식 제거
    - FormUserDetailsService를 구현하여 데이터베이스 연동 방식으로 UserDetailsService 설정
    - AccountContext 클래스 추가: UserDetails를 구현해 사용자 정보를 확장
    - 기존 InMemoryUserDetailsManager 방식 삭제

--- src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
-package io.security.springsecuritymaster.config;
+package io.security.springsecuritymaster.security.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -15,23 +16,20 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon--*").permitAll().requestMatchers("/", "/signup").permitAll().anyRequest().authenticated()).formLogin(form -> form.loginPage("/login").permitAll());
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon--*").permitAll().requestMatchers("/", "/signup").permitAll().anyRequest().authenticated()).formLogin(form -> form.loginPage("/login").permitAll()).userDetailsService(userDetailsService);
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
-        return new InMemoryUserDetailsManager(user);
     }
 
 }
